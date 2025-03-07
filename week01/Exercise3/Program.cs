b82b2b8309ac75ecb9ef45d92eaa4ff5bef04665
Camilo Supelano <edison.supelano@gmail.com>
2025-03-06 23:09:34 -0500

    Created Magic Loop number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Guess this magic number: ");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        }
+            
     }
 }
