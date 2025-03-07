9d231006652306a5e9a2898fccc0f29850f05055
Camilo Supelano <edison.supelano@gmail.com>
2025-03-06 22:29:22 -0500

    Created conditional statements.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+
+        Console.WriteLine("What is your grade?");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if(grade >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed with a {letter} grade!!");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter} continue studying so you get it next time");
+        }
     }
 }
