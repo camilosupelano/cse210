9a35a78a5014c96e37fb3dfa1adb8afc64789e8b
Camilo Supelano <edison.supelano@gmail.com>
2025-03-31 21:48:51 -0500

    Assigment program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        
+        Assignment assignHw = new Assignment ("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignHw.GetSummary());
+        Console.WriteLine();
+        
+        MathAssignment mathHw = new MathAssignment("John Doe", "Algebra", "7.3", "8-19"); Console.WriteLine(mathHw.GetSummary());
+        Console.WriteLine(mathHw.GetHomeworkList());
+        Console.WriteLine();
+        
+        
+        WritingAssignment writingHw = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingHw.GetSummary());
+        Console.WriteLine(writingHw.GetWritingInformation());
+        Console.WriteLine();
+        
+
     }
 }
