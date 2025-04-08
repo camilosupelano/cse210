24a169723776f4713f17cdbc753edd9b9bd85b87
Camilo Supelano <edison.supelano@gmail.com>
2025-04-07 20:27:35 -0500

    Shapes program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 7);
+        Shape rectangle = new Rectangle("Green", 8, 12);
+        Shape circle = new Circle("Blue", 6);
+
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
